87f9e48df8979dad3d14449081b608c629123832
Fadil Fahrduin <fadilfahrudin32@gmail.com>
2023-01-27 15:54:32 +0700

    [update] link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { LogoReact } from "../../assets/icon";
 import {
   Behance,
   Dribbble,
@@ -22,22 +21,46 @@ const Footer = () => {
         <p className="email">fadilfahrudin32@gmail.com</p>
       </div>
       <div className="socialMedia">
-        <a href="/#">
+        <a
+          href="https://dribbble.com/InitialF/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Dribbble} alt="icon" className="logo" />
         </a>
-        <a href="/#">
+        <a
+          href="https://www.behance.net/fadilfahrudin"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Behance} alt="icon" className="logo" />
         </a>
-        <a href="/#">
+        <a
+          href="https://github.com/fadilfahrudin"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Github} alt="icon" className="logo" />
         </a>
-        <a href="/#">
+        <a
+          href="https://www.instagram.com/fadilfahrudin/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Instagram} alt="icon" className="logo" />
         </a>
-        <a href="/#">
+        <a
+          href="https://www.linkedin.com/in/fadillahfahrudin"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Linkedin} alt="icon" className="logo" />
         </a>
-        <a href="/#">
+        <a
+          href="https://www.youtube.com/@fadilfahrudin3807"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Youtube} alt="icon" className="logo" />
         </a>
       </div>

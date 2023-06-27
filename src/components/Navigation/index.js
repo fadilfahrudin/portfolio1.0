d29d7187a625f98cb8b4256534930bb7ae45e145
fadilfahrudin <fadilfahrudin32@gmail.com>
2023-06-27 11:32:39 +0700

    new navigation

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import LoadingBar from "react-top-loading-bar";
+import useScrollListLiner from "../../utils/Hooks/useScrollListener";
+import { useNavigate } from "react-router-dom";
+import { LogoF3 } from "../../assets/icon";
+const Header = () => {
+	const [navList, setNavList] = useState([]);
+	const scroll = useScrollListLiner();
+	const [progress, setProgress] = useState(0);
+
+	useEffect(() => {
+		const _classList = [];
+
+		if (scroll.y > 50 && scroll.y - scroll.lastY > 0) {
+			_classList.push("nav-bar--hidden2");
+		}
+		setNavList(_classList);
+	}, [scroll.y, scroll.lastY]);
+
+	const menus = [
+		{
+			title: "Home",
+			link: "/",
+		},
+		{
+			title: "About",
+			link: "/about",
+		},
+		{
+			title: "Project",
+			link: "/projects",
+		},
+	];
+
+	const naviagte = useNavigate();
+	const currenturl = window.location.pathname;
+
+	const clicked = (menu) => {
+		setProgress(progress + 100);
+		return naviagte(menu.link);
+	};
+
+	return (
+		<>
+			<LoadingBar
+				color='#f2a365'
+				progress={progress}
+				onLoaderFinished={() => setProgress(0)}
+			/>
+
+			<nav className={`${navList.join(" ")}`}>
+				<div className='nav-brand'>
+					<a href='/'>
+						<img src={LogoF3} alt='Fadil Fahrudin' className='logoHeader' />
+					</a>
+				</div>
+				<div className='nav'>
+					<ul>
+						{menus.map((menu, index) => {
+							return (
+								<li
+									onClick={() => clicked(menu, index)}
+									key={index}
+									className={menu.link === currenturl ? "active" : "menuNav"}>
+									{menu.title}
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
+};
+
+export default Header;
